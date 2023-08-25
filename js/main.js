7b944e33351d6b2db6b11e8da74afcd6ea69f7c0
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-08-25 15:44:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,7 +52,8 @@ $(document).ready(function() {
         "Peacock Amapiano Remix",
         "Uncle Waffles  Peacock Revisit ft Ice Beats Slide  Sbuda Maleather Official Video",
         "Jimmy Maradona Quayr Musiq Mellow  Sleazy  Wena Wa Pallwa Official Audio Ft Chcco Leemckrazy",
-        "Makhadzi Entertainment  Movie Official Lyrics Video feat Ntate Stunna Fortunator  Dj Gun Do"
+        "Makhadzi Entertainment  Movie Official Lyrics Video feat Ntate Stunna Fortunator  Dj Gun Do",
+        "Ice Beats Slides x Sbuda Maleather  Qopetsa Ft Boi Bizza Official Audio"
     ];
 
 
