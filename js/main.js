ce9084a596eac2756fb90ed23df35c832fc978b8
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2023-12-21 00:01:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,6 +88,7 @@ $(document).ready(function() {
         "Wanitwa Mos x Nkosazana Daughter  Master KG  Keneilwe Feat Dalom Kids",
         "Killorbeezbeatz  uDecember Ksasa Official Audio",
         "Wanitwa Mos  Master KG   Dali Nguwe ft Nkosazana Daughter Basetsana Obeey Amor Official Video New"
+        "DJ EDDIE NOMATHEMBA REVISIT 2023"
     ];
 
 
